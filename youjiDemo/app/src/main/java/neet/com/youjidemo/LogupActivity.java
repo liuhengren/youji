0396650583f7d6c3ac71eb94a882fed0d40f566b
liuhengren <1016406999@qq.com>
2018-12-04 17:03:21 +0800

    update12-4

--- youjiDemo/app/src/main/java/neet/com/youjidemo/LogupActivity.java
+++ youjiDemo/app/src/main/java/neet/com/youjidemo/LogupActivity.java
@@ -3,11 +3,12 @@ package neet.com.youjidemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class login extends AppCompatActivity {
+public class LogupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.module_activity_login);
+        setContentView(R.layout.logup_activity);
+        //getActionBar().hide();
     }
 }
