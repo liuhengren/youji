031d0b0125d3c30f995d01a5ff97908e4ac82fd1
Liangqiwen123 <651838281@qq.com>
2018-12-07 08:11:15 +0800

    0.0

---
+++ youjiDemo/app/src/main/java/neet/com/youjidemo/view/DetailActivity.java
@@ -0,0 +1,15 @@
+package neet.com.youjidemo.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import neet.com.youjidemo.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
