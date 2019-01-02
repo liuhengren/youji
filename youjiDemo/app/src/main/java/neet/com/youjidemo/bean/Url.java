fa008e3ee974b26d0c6a45ed55d4ecde2aaf1946
liuhengren <1016406999@qq.com>
2019-01-02 20:23:40 +0800

    Update Url.java

--- youjiDemo/app/src/main/java/neet/com/youjidemo/bean/Url.java
+++ youjiDemo/app/src/main/java/neet/com/youjidemo/bean/Url.java
@@ -1,13 +1,8 @@
 package neet.com.youjidemo.bean;
 
 public class Url {
-<<<<<<< HEAD
+
     public static  final String mURL="http://10.7.89.200:8080/youjiServer/";
     public static  final  String MIMAGEURL="http://10.7.89.200:8080/youjiServer/upload/";
-=======
 
-    public static  final String mURL="http://10.222.189.201:8080/youjiServer/";
-    public static  final  String MIMAGEURL="http://10.222.189.201:8080/youjiServer/upload/";
-
->>>>>>> 2b541977f4aa3a4f41fb4b9efe36f229798654b2
 }
